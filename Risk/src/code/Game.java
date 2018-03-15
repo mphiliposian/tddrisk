a34b39c05076a54acceda361eb22f8eb9d3badd5
Quinn G Schaffer <schaffqg@rose-hulman.edu>
2018-03-15 16:46:59 -0400

    testAddPlayerToGame passes

--- Risk/src/code/Game.java
+++ Risk/src/code/Game.java
@@ -1,9 +1,21 @@
 package code;
 
+import java.util.ArrayList;
+
 public class Game {
 
+	private ArrayList<Player> p;
+	
+	public Game() {
+		this.p = new ArrayList<Player>();
+	}
+	
 	public boolean gameIsWon() {
 		return true;
+	}
+
+	public void addPlayer(Player p) {
+		
 	}
 
 }
--- Risk/src/test/TestGameState.java
+++ Risk/src/test/TestGameState.java
@@ -2,16 +2,28 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
 import code.Game;
+import code.Player;
 
 class TestGameState {
 
+	@Test 
+	void testAddPlayerToGame() {
+		Game g = new Game();
+		Player p = new Player();
+		g.addPlayer(p);
+	}
+	
+	/*
 	@Test
 	void testWinGameState() {
 		Game g = new Game();
+		Player p = EasyMock.mock(Player.class);
+		EasyMock.expect(p.getNumOfTerritories()).andReturn(42);
 		assertTrue(g.gameIsWon());
 	}
-
+	*/
 }
