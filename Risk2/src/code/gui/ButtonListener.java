3db6359c77c68c89015b9835e49093ffc733b7f5
gearjt <gearjt@rose-hulman.edu>
2018-03-26 16:45:35 -0400

    added actionlistener for buttons

---
+++ Risk2/src/code/gui/ButtonListener.java
@@ -0,0 +1,14 @@
+package code.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
