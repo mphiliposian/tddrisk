de208c2ac78e76eef72b140e421c9e493d885c37
mphiliposian <philipmj@rose-hulman.edu>
2018-04-30 17:33:44 -0400

    removed unnecessary import

--- Risk/src/test/game/TestAllocateUnitPhase.java
+++ Risk/src/test/game/TestAllocateUnitPhase.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
