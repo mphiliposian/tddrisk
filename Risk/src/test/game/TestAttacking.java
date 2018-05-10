65b77f4f87970bf6d53b3c7f7c85779d5e051451
Quinn G Schaffer <schaffqg@rose-hulman.edu>
2018-05-10 12:45:03 -0400

    all tests fail in TestAttacking

--- Risk/src/test/game/TestAttacking.java
+++ Risk/src/test/game/TestAttacking.java
@@ -38,6 +38,27 @@ public class TestAttacking {
 		EasyMock.verify(ui);
 	}
 	
+	@Test
+	public void attackCountryNotConnected() {
+		RiskUI ui = EasyMock.niceMock(RiskUI.class);
+		EasyMock.expect(ui.playerCountPrompt()).andReturn(3);
+		Game game = new Game(ui);
+		ArrayList<String> territoriesConnectedToNA1 = new ArrayList<String>();
+		territoriesConnectedToNA1.add("NA2");
+		ArrayList<String> territoriesConnectedToNA3 = new ArrayList<String>();
+		territoriesConnectedToNA3.add("NA2");
+		Territory player1Territory = new Territory("NA1", "murica", 2, null, 0, 0);
+		Territory player2Territory = new Territory("NA3", "murica2", 1, null, 0, 0);
 
+		EasyMock.expect(ui.territoryPrompt(EasyMock.anyString())).andReturn(player1Territory);
+		EasyMock.expect(ui.territoryPrompt(EasyMock.anyString())).andReturn(player2Territory);
+		
+		EasyMock.replay(ui);
+		game.createPlayers();
+		
+
+		assertFalse(game.attack());
+		EasyMock.verify(ui);
+	}
 
 }
