c1d6df90932676913d130b00bd0f42519a5dd323
gearjt <gearjt@rose-hulman.edu>
2018-05-11 19:23:06 -0400

    linted, cleaned, and formatted riskui and territory selectoin listener

--- Risk/src/code/gui/RiskUI.java
+++ Risk/src/code/gui/RiskUI.java
@@ -2,28 +2,19 @@ package code.gui;
 
 import java.awt.Color;
 import java.util.List;
-
 import code.game.Player;
 import code.game.Territory;
 
 public interface RiskUI {
 
-	void initializeUI(List<Territory> territories);
-	
+	void initializeUI(List <Territory> territories);
 	int playerCountPrompt();
-	
-	void createPlayerDisplay(List<Player> players);
-	
-	void createMapDisplay(List<Territory> territories);
-	
+	void createPlayerDisplay(List <Player> players);
+	void createMapDisplay(List <Territory> territories);
 	void updatePlayerDisplay(int activePlayer);
-	
 	void updateTerritoryDisplay(Territory territory, Color color);
-	
 	Territory territoryPrompt(String message);
-	
 	void setCancelButtonVisible(boolean isVisible);
-	
 	int reinforcementCountPrompt(int max, String title, String message);
-	
+
 }
--- Risk/src/code/gui/TerritorySelectionListener.java
+++ Risk/src/code/gui/TerritorySelectionListener.java
@@ -2,15 +2,13 @@ package code.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-
 import code.game.Territory;
 
 public class TerritorySelectionListener implements ActionListener {
+
 	private RiskGUI gui;
 	private Territory territory;
-	
+
 	public TerritorySelectionListener(RiskGUI gui, Territory territory) {
 		this.gui = gui;
 		this.territory = territory;
@@ -20,5 +18,4 @@ public class TerritorySelectionListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		gui.selectTerritory(this.territory);
 	}
-
 }
