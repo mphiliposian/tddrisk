18006cd160f13a4516245b205aeab205dd709ee4
gearjt <gearjt@rose-hulman.edu>
2018-05-11 18:13:33 -0400

    refactored messages and player

--- Risk/src/code/game/Messages.java
+++ Risk/src/code/game/Messages.java
@@ -4,11 +4,12 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "code.game.messages"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+private static final String BUNDLE_NAME = "code.game.messages"; //$NON-NLS-1$
+private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
 	private Messages() {
+
 	}
 
 	public static String getString(String key) {
