7752112606cbf1128dffcb667e26a8b51614f792
Quinn G Schaffer <schaffqg@rose-hulman.edu>
2018-05-18 17:34:33 -0400

    uniqueVerify fails

--- Risk/src/test/game/TestCardLogic.java
+++ Risk/src/test/game/TestCardLogic.java
@@ -206,5 +206,28 @@ public class TestCardLogic {
 		assertFalse(game.verifyCards(cards));
 	}
 	
+	@Test 
+	public void uniqueVerify() {
+		RiskUI ui = EasyMock.niceMock(RiskUI.class);
+		EasyMock.expect(ui.playerCountPrompt()).andReturn(3);
+		Game game = new Game(ui);
+		Territory territory1 = new Territory("NA1", "murica", 10, territoriesConnectedToNA1, 0, 0);
+		Territory territory2 = new Territory("NA2", "murica2", 1, territoriesConnectedToNA2, 0, 0);
+		Territory territory3 = new Territory("NA3", "murica2", 1, territoriesConnectedToNA2, 0, 0);
+		
+		Card infantry1 = new Card(territory1, Card.CardType.Infantry);
+		Card infantry2 = new Card(territory1, Card.CardType.Infantry);
+		Card infantry3 = new Card(territory1, Card.CardType.Infantry);
+		Card calvary = new Card(territory2, Card.CardType.Calvary);
+		Card artillery = new Card(territory3, Card.CardType.Artillery);
+		
+		List<Card> cards = new ArrayList<>();
+		cards.add(infantry1);
+		cards.add(calvary);
+		cards.add(artillery);
+		
+		assertTrue(game.verifyCards(cards));
+	}
+	
 	
 }
