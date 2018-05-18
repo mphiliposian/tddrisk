7524b8082ca45a7d4f9b6f7341a779d322e2c46c
Quinn G Schaffer <schaffqg@rose-hulman.edu>
2018-05-18 13:16:14 -0400

    PlayerCancelsAttack passes

--- Risk/src/test/game/TestBattle.java
+++ Risk/src/test/game/TestBattle.java
@@ -1,6 +1,7 @@
 package test.game;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -130,6 +131,27 @@ public class TestBattle {
 		EasyMock.replay(ui);
 		assertTrue(game.battle(player1Territory, player2Territory));
 		assertEquals(player1Territory.getYield(), 1);
+		assertEquals(player2Territory.getYield(), 4);
+		EasyMock.verify(ui);
+	}
+	
+	@Test
+	public void PlayerCancelsAttack() {
+		RiskUI ui = EasyMock.niceMock(RiskUI.class);
+		Player player = new Player(0);
+		Set <Territory> ownedTerritories = new HashSet <> ();
+		Territory player1Territory = new Territory("NA1", "murica", 3, territoriesConnectedToNA1, 0, 0);
+		Territory player2Territory = new Territory("NA2", "murica2", 4, territoriesConnectedToNA2, 0, 0);
+		ownedTerritories.add(player1Territory);
+		ArrayList <Player> players = new ArrayList <> ();
+		players.add(player);
+		Map <Player, Set <Territory>> playersTerritories = new HashMap <> ();
+		playersTerritories.put(player, ownedTerritories);
+		Game game = new Game(ui, players, playersTerritories, 4);
+		EasyMock.expect(ui.reinforcementCountPrompt(2, "Select number of units to attack with.", "Reinforcements", JOptionPane.OK_CANCEL_OPTION)).andReturn(-1);
+		EasyMock.replay(ui);
+		assertFalse(game.battle(player1Territory, player2Territory));
+		assertEquals(player1Territory.getYield(), 3);
 		assertEquals(player2Territory.getYield(), 4);
 		EasyMock.verify(ui);
 	}
