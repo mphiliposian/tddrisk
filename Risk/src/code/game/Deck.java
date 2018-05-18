3d17274aa6dc82bd1a656e43e22a512d8b44cdd0
Quinn G Schaffer <schaffqg@rose-hulman.edu>
2018-05-18 16:34:41 -0400

    Forgot to push the deck class

---
+++ Risk/src/code/game/Deck.java
@@ -0,0 +1,63 @@
+package code.game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Stack;
+
+import code.game.Card.CardType;
+
+public class Deck {
+
+	Stack<Card> drawPile = new Stack<>();
+	Stack<Card> discardPile = new Stack<>();
+	Random rand;
+
+	public Deck(List<Territory> territories, Random rand) {
+		this.rand = rand;
+		this.drawPile = initializeDeck(territories);
+	}
+
+	public Stack<Card> initializeDeck(List<Territory> territories) {
+		Stack<Card> deck = new Stack<>();
+		for(int i = 0; i < 42; i++) {
+			CardType type = CardType.values()[i%3];
+			Card card = new Card(territories.get(i), type);
+			deck.add(card);
+		}
+		for(int i = 0; i < 2; i++) {
+			Card card = new Card(null, CardType.WILD);
+			deck.add(card);
+		}
+		Collections.shuffle(deck,this.rand);
+		return deck;
+	}
+
+	public Card drawCard() {
+		if (!this.isEmpty()) {
+			return drawPile.pop();
+		}
+		this.reShuffleDiscard();
+		return drawPile.pop();
+	}
+
+	private void reShuffleDiscard() {
+		Collections.shuffle(discardPile, rand);
+		for(Card card : discardPile){
+			drawPile.push(discardPile.pop());
+		}
+	}
+	
+	public void discard(Card card) {
+		discardPile.push(card);
+	}
+
+	public boolean isEmpty() {
+		return drawPile.isEmpty();
+	}
+}
+
+
+
