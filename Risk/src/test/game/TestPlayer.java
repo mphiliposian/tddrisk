1f4e492a75eb9b6db270fc9ca67f428117e118bc
Quinn G Schaffer <schaffqg@rose-hulman.edu>
2018-05-18 22:25:24 -0400

    addCardToPlayer passes

--- Risk/src/test/game/TestPlayer.java
+++ Risk/src/test/game/TestPlayer.java
@@ -1,8 +1,13 @@
 package test.game;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import code.game.Card;
 import code.game.Player;
+import code.game.Territory;
 
 public class TestPlayer {
 
@@ -13,4 +18,15 @@ public class TestPlayer {
 			assertEquals(player.getColor(), Player.COLORS[i]);
 		}
 	}
+
+	@Test
+	public void addCardToPlayer() {
+		Player player = new Player(0);
+		Card card = new Card(new Territory("End", "null", 0, null, 0, 0), Card.CardType.Artillery);
+		assertEquals(player.getHand().size(),0);
+		assertTrue(player.addCardToHand(card));
+		assertEquals(player.getHand().size(),1);
+	}
+
+
 }
